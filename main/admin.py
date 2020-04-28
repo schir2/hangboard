196cc83c3672f8e9d46f4b35e2d4e15d94dde644
Marek Schir <schir2@gmail.com>
2020-04-28 01:50:08 -0400

    Updated models to different hands. Added Workout models.py

--- main/admin.py
+++ main/admin.py
@@ -3,25 +3,39 @@ from django.contrib import admin
 from main.models import Hold
 from main.models import Hangboard
 from main.models import HoldType
+from main.models import Workout
 from main.models import WorkoutSet
 from main.models import Exercise
 from main.models import Material
 
 
-class SimpleModelAdmin(admin.ModelAdmin):
+class AutoAddAccountModelAdmin(admin.ModelAdmin):
+    def save_model(self, request, obj, form, change):
+        if not obj.pk:
+            # Only set added_by during the first save.
+            obj.account = request.user
+        super().save_model(request, obj, form, change)
+
+    class Meta:
+        abstract = True
+
+
+class SimpleModelAdmin(AutoAddAccountModelAdmin):
     list_display = ('name', 'slug', 'description', 'created', 'updated', 'account')
+    fields = ('name', 'slug', 'description', 'account')
 
     class Meta:
         abstract = True
 
 
 @admin.register(Hold)
-class HoldAdmin(SimpleModelAdmin):
-    pass
+class HoldAdmin(AutoAddAccountModelAdmin):
+    list_display = ('name', 'slug', 'hold_type', 'hangboard', 'description', 'created', 'updated', 'account')
+    fields = ('name', 'slug', 'hold_type', 'hangboard', 'description', 'account')
 
 
 @admin.register(Exercise)
-class ExcerciseAdmin(SimpleModelAdmin):
+class ExerciseAdmin(SimpleModelAdmin):
     pass
 
 
@@ -33,3 +47,20 @@ class MaterialAdmin(SimpleModelAdmin):
 @admin.register(HoldType)
 class HoldTypeAdmin(SimpleModelAdmin):
     pass
+
+
+@admin.register(Hangboard)
+class HangboardAdmin(AutoAddAccountModelAdmin):
+    list_display = ('name', 'slug', 'material', 'description', 'created', 'updated', 'account')
+    fields = ('name', 'slug', 'material', 'description', 'account')
+
+
+@admin.register(WorkoutSet)
+class WorkoutSetAdmin(AutoAddAccountModelAdmin):
+    list_display = ('name',)
+
+
+@admin.register(Workout)
+class WorkoutAdmin(AutoAddAccountModelAdmin):
+    list_display = ('name', 'slug', 'description', 'created', 'updated', 'logged', 'account')
+    fields = ('name', 'slug', 'description', 'account')
--- main/models.py
+++ main/models.py
@@ -1,17 +1,15 @@
 from django.db import models
 from django.contrib.auth import get_user_model
 from django.utils.text import slugify
-
-
-# Create your models here.
+from django.core.validators import MinValueValidator, MaxValueValidator
 
 
 class SimpleModel(models.Model):
-    slug = models.SlugField()
+    slug = models.SlugField(blank=True, null=True)
     name = models.CharField(max_length=100)
     description = models.TextField(blank=True, default='')
     custom = models.BooleanField(default=True)
-    account = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
+    account = models.ForeignKey(get_user_model(), on_delete=models.CASCADE, blank=True, null=True)
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
 
@@ -51,14 +49,31 @@ class Hangboard(SimpleModel):
 class Hold(SimpleModel):
     hangboard = models.ForeignKey(Hangboard, on_delete=models.CASCADE)
     hold_type = models.ForeignKey(HoldType, on_delete=models.CASCADE)
-    size = models.IntegerField(default=20, null=True, blank=True)
+    size = models.PositiveIntegerField(default=20, null=True, blank=True)
     default_fingers = models.IntegerField(default=4)
 
 
+class Workout(SimpleModel):
+    logged = models.DateTimeField(auto_now_add=True)
+
+
 class WorkoutSet(SimpleModel):
-    hold = models.ForeignKey(Hold, on_delete=models.CASCADE)
-    fingers = models.IntegerField(default=4)
-    rest_interval = models.IntegerField(default=60)
+    left_hold = models.ForeignKey(Hold, related_name='left_hold', on_delete=models.CASCADE, null=True, blank=True)
+    right_hold = models.ForeignKey(Hold, related_name='right_hold', on_delete=models.CASCADE, null=True, blank=True)
+    left_fingers = models.PositiveIntegerField(default=4, validators=[MinValueValidator(1), MaxValueValidator(5)])
+    right_fingers = models.PositiveIntegerField(default=4, validators=[MinValueValidator(1), MaxValueValidator(5)])
+    rest_interval = models.PositiveIntegerField(default=60)
+    duration = models.PositiveIntegerField(default=0)
     weight = models.IntegerField(default=0)
-    reps = models.IntegerField(default=1)
+    reps = models.PositiveIntegerField(default=1)
     exercise = models.ForeignKey(Exercise, on_delete=models.CASCADE)
+    workout = models.ForeignKey(Workout, on_delete=models.CASCADE)
+
+    def save(self, *args, **kwargs):
+        if not self.left_hold and not self.right_hold:
+            raise ValueError('No holds selected')
+        if not self.slug:
+            self.slug = slugify(self.name, allow_unicode=True)
+        if not self.description:
+            self.description = self.name
+        super().save(*args, *kwargs)
