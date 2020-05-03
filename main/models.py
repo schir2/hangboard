656643979c0b58cc359c33fe8f8da615191cb0ea
Marek Schir <schir2@gmail.com>
2020-05-03 16:28:36 -0400

    Added left and right hand hold to BaseWorkoutSet abstract model.

--- main/models.py
+++ main/models.py
@@ -9,12 +9,18 @@ class SimpleModel(models.Model):
     name = models.CharField(max_length=100)
     description = models.TextField(blank=True, default='')
     custom = models.BooleanField(default=True)
-    account = models.ForeignKey(get_user_model(), on_delete=models.CASCADE, blank=True, null=True)
+    climber = models.ForeignKey(get_user_model(), on_delete=models.CASCADE, blank=True, null=True)
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
 
     def __repr__(self):
-        return f'{self.name}'
+        return f'{self.__class__.__name__}(' \
+               f'name={self.name},' \
+               f'slug={self.slug},' \
+               f'custom={self.custom},' \
+               f'climber={self.climber},' \
+               f'created={self.created},' \
+               f'updated={self.updated}'
 
     def __str__(self):
         return f'{self.name}'
@@ -53,7 +59,31 @@ class Hold(SimpleModel):
     default_fingers = models.IntegerField(default=4)
 
 
+class BaseWorkout(SimpleModel):
+    note = models.TextField(blank=True, null=True, default='')
+    completed = models.BooleanField(default=False)
+
+    class Meta:
+        abstract = True
+
+
+class Workout(BaseWorkout):
+    scheduled = models.DateTimeField(auto_now_add=True)
+    difficulty = models.PositiveIntegerField(
+        blank=True,
+        null=True,
+        validators=[MinValueValidator(1), MaxValueValidator(5)]
+    )
+
+
+class TemplateWorkout(BaseWorkout):
+    pass
+
+
 class BaseWorkoutSet(SimpleModel):
+    name = models.CharField(max_length=100, blank=True, null=True)
+    left_hold = models.ForeignKey(Hold, related_name='left_hold', on_delete=models.CASCADE, null=True, blank=True)
+    right_hold = models.ForeignKey(Hold, related_name='right_hold', on_delete=models.CASCADE, null=True, blank=True)
     left_fingers = models.PositiveIntegerField(default=4, validators=[MinValueValidator(1), MaxValueValidator(5)])
     right_fingers = models.PositiveIntegerField(default=4, validators=[MinValueValidator(1), MaxValueValidator(5)])
     rest_interval = models.PositiveIntegerField(default=60)
@@ -66,38 +96,23 @@ class BaseWorkoutSet(SimpleModel):
     class Meta:
         abstract = True
 
+    def save(self, *args, **kwargs):
+        if not self.left_hold and not self.right_hold:
+            raise ValueError('No holds selected')
+        if not self.left_hold:
+            self.left_fingers = None
 
-class BaseWorkout(SimpleModel):
-    note = models.TextField(blank=True, null=True, default='')
-    completed = models.BooleanField(default=False)
-
-    class Meta:
-        abstract = True
-
-
-class Workout(BaseWorkout):
-    scheduled = models.DateTimeField(auto_now_add=True)
-    difficulty = models.PositiveIntegerField(blank=True, null=True,
-                                             validators=[MinValueValidator(1), MaxValueValidator(5)])
-
-
-class TemplateWorkout(BaseWorkout):
-    pass
+        if not self.slug:
+            self.slug = slugify(self.name, allow_unicode=True)
+        if not self.description:
+            self.description = self.name
+        super().save(*args, *kwargs)
 
 
 class WorkoutSet(BaseWorkoutSet):
     workout = models.ForeignKey(Workout, on_delete=models.CASCADE)
     left_hold = models.ForeignKey(Hold, related_name='left_hold', on_delete=models.CASCADE, null=True, blank=True)
     right_hold = models.ForeignKey(Hold, related_name='right_hold', on_delete=models.CASCADE, null=True, blank=True)
-
-    def save(self, *args, **kwargs):
-        if not self.left_hold and not self.right_hold:
-            raise ValueError('No holds selected')
-        if not self.slug:
-            self.slug = slugify(self.name, allow_unicode=True)
-        if not self.description:
-            self.description = self.name
-        super().save(*args, *kwargs)
 
 
 class TemplateWorkoutSet(BaseWorkoutSet):
