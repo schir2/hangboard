1669e62fa1c28277c921e01de0ae09b67a95611b
Marek Schir <schir2@gmail.com>
2020-05-06 01:30:09 -0400

    Setting up initial template files for views

--- main/urls.py
+++ main/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 
 from main.views import workout_set_form_view
+from main.views import WorkoutListView
 
 urlpatterns = [
     path('forms/workout_set', workout_set_form_view, name='workout_set_form'),
+    path('', WorkoutListView.as_view(), name='workout_list'),
 ]
--- main/views/__init__.py
+++ main/views/__init__.py
@@ -1,1 +1,38 @@
+from abc import ABCMeta, abstractmethod
+from django.views.generic.base import TemplateView
+from django.views.generic.list import ListView
+from django.core import paginator
+from django.contrib.auth.decorators import login_required
+from django.utils.decorators import method_decorator
+
 from .forms import *
+from main.models import Hangboard
+from main.models import Workout
+
+
+class BaseView(TemplateView, metaclass=ABCMeta):
+
+    @abstractmethod
+    def get(self, request, *args, **kwargs):
+        pass
+
+    @abstractmethod
+    def post(self, request, *args, **kwargs):
+        pass
+
+    def pagination(self, data, results_per_page: int = 20) -> paginator:
+        pass
+
+
+@method_decorator(login_required, 'dispatch')
+class WorkoutListView(ListView):
+    model = Workout
+    paginate_by = 20
+    context_object_name = 'workouts'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        return context
+
+    def get_queryset(self):
+        return Workout.objects.filter(climber=self.request.user)
