cc6b740ba5a56dd197cb16691d74d76d97f95dc8
Marek Schir <schir2@gmail.com>
2020-05-07 19:23:43 -0400

    Added url for workout details

--- main/urls.py
+++ main/urls.py
@@ -2,8 +2,10 @@ from django.urls import path
 
 from main.views import workout_set_form_view
 from main.views import WorkoutListView
+from main.views import WorkoutDetailView
 
 urlpatterns = [
     path('forms/workout_set', workout_set_form_view, name='workout_set_form'),
     path('', WorkoutListView.as_view(), name='workout_list'),
+    path('<slug:slug>/', WorkoutDetailView.as_view(), name='workout_detail'),
 ]
