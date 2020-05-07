d56315abd62e627159af8bfc87c38fc9c4a78d85
Marek Schir <schir2@gmail.com>
2020-05-07 19:20:31 -0400

    Moved UserManager from models to managers.py

---
+++ climbers/managers.py
@@ -0,0 +1,41 @@
+from django.contrib.auth.models import BaseUserManager
+
+
+class UserManager(BaseUserManager):
+    use_in_migrations = True
+
+    def _create_user(self, username: str, email: str, password: str, **extra_fields):
+        """
+        Create and save a user with the given handle, email, and password.
+        """
+        if not email:
+            raise ValueError('The given email must be set')
+        if not username:
+            raise ValueError('The given username must be set')
+        email = self.normalize_email(email)
+        username = self.model.normalize_username(username)
+        user = self.model(username=username, email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+
+        current_user = self.model.objects.get(email=email)
+        current_user.profile
+        current_user.preference
+        current_user.measurement
+        return user
+
+    def create_user(self, username: str, email: str = None, password: str = None, **extra_fields):
+        extra_fields.setdefault('is_staff', False)
+        extra_fields.setdefault('is_superuser', False)
+        return self._create_user(username, email, password, **extra_fields)
+
+    def create_superuser(self, username: str, email: str = None, password: str = None, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+
+        if extra_fields.get('is_staff') is not True:
+            raise ValueError('Superuser must have is_staff=True.')
+        if extra_fields.get('is_superuser') is not True:
+            raise ValueError('Superuser must have is_superuser=True.')
+
+        return self._create_user(username, email, password, **extra_fields)
