bae4db9c8d10f3f2eaf665fe0e2b5989e024d6f2
Marek Schir <schir2@gmail.com>
2020-05-07 19:21:38 -0400

    Moved UserManager to managers.py

--- climbers/models.py
+++ climbers/models.py
@@ -1,49 +1,10 @@
 from django.db import models
-from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
 from django.contrib.auth.validators import UnicodeUsernameValidator
 from django.utils.translation import gettext_lazy as _
 from django.utils import timezone
 from annoying.fields import AutoOneToOneField
-
-
-class UserManager(BaseUserManager):
-    use_in_migrations = True
-
-    def _create_user(self, username: str, email: str, password: str, **extra_fields):
-        """
-        Create and save a user with the given handle, email, and password.
-        """
-        if not email:
-            raise ValueError('The given email must be set')
-        if not username:
-            raise ValueError('The given username must be set')
-        email = self.normalize_email(email)
-        username = self.model.normalize_username(username)
-        user = self.model(username=username, email=email, **extra_fields)
-        user.set_password(password)
-        user.save(using=self._db)
-
-        current_user = self.model.objects.get(email=email)
-        current_user.profile
-        current_user.preference
-        current_user.stat
-        return user
-
-    def create_user(self, username: str, email: str = None, password: str = None, **extra_fields):
-        extra_fields.setdefault('is_staff', False)
-        extra_fields.setdefault('is_superuser', False)
-        return self._create_user(username, email, password, **extra_fields)
-
-    def create_superuser(self, username: str, email: str = None, password: str = None, **extra_fields):
-        extra_fields.setdefault('is_staff', True)
-        extra_fields.setdefault('is_superuser', True)
-
-        if extra_fields.get('is_staff') is not True:
-            raise ValueError('Superuser must have is_staff=True.')
-        if extra_fields.get('is_superuser') is not True:
-            raise ValueError('Superuser must have is_superuser=True.')
-
-        return self._create_user(username, email, password, **extra_fields)
+from climbers.managers import UserManager
 
 
 class Climber(AbstractBaseUser, PermissionsMixin):
