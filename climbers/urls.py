19ce4049d2b14b0806d5993a6795fe376b57e9e9
Marek Schir <schir2@gmail.com>
2020-05-22 23:35:54 -0400

    Setup base.html and scss.

--- climbers/urls.py
+++ climbers/urls.py
@@ -1,8 +1,12 @@
 from django.urls import path, include
 from climbers.views import profile_view
+from climbers.views import preferences_view
+from climbers.views import measurements_view
 
 urlpatterns = [
     path('<str:username>/', profile_view),
-    path('<str:username>/profile/', profile_view, name='profile_view'),
+    path('<str:username>/profile/', profile_view, name='profile'),
+    path('<str:username>/profile/', preferences_view, name='preferences'),
+    path('<str:username>/profile/', measurements_view, name='measurements'),
     path('<str:username>/workouts/', include('workouts.urls')),
 ]
--- climbers/views.py
+++ climbers/views.py
@@ -6,4 +6,24 @@ from django.contrib.auth.decorators import login_required
 def profile_view(request, *args, **kwargs):
     template_name = 'climbers/profile.html'
     context = dict()
+    context['title'] = 'Profile'
+    context['description'] = 'User Profile'
+    return render(request, template_name=template_name, context=context)
+
+
+@login_required
+def preferences_view(request, *args, **kwargs):
+    template_name = 'climbers/preferences.html'
+    context = dict()
+    context['title'] = 'Preferences'
+    context['description'] = 'User Preferences'
+    return render(request, template_name=template_name, context=context)
+
+
+@login_required
+def measurements_view(request, *args, **kwargs):
+    template_name = 'climbers/measurements.html'
+    context = dict()
+    context['title'] = 'Measurements'
+    context['description'] = 'User Measurements'
     return render(request, template_name=template_name, context=context)
